62d8e280749540ed397e3109c3a7fa9b6e275d14
Charanreddy0007 <mulicharanreddy@gmail.com>
2026-07-13 23:49:25 +0530

    Added config.py

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+# Modify according to yours 
+OWNER = os.getenv("OWNER")
+
+REPO = os.getenv("REPO")
+
+DATABASE = "database/database.db"
+
+TOKEN = os.getenv("TOKEN_GITHUB")
+
+URL = f"https://api.github.com/repos/{OWNER}/{REPO}/contents/"
+
+QUESTION_URL = f"https://leetcode.com/problems/"
+
+GRAPHQL_URL = "https://leetcode.com/graphql"
+
+COOKIES = {
+        'csrftoken' : os.getenv("CSRFTOKEN"),
+        'LEETCODE_SESSION' : os.getenv("LEETCODE_SESSION"),
+}
+
+EXTENSIONS = {
+        "python": ".py",
+        "python3": ".py",
+        "java": ".java",
+        "c": ".c",
+        "c++": ".cpp",
+        "cpp": ".cpp",
+        "c#": ".cs",
+        "javascript": ".js",
+        "typescript": ".ts",
+        "go": ".go",
+        "kotlin": ".kt",
+        "swift": ".swift",
+        "rust": ".rs",
+        "ruby": ".rb",
+        "php": ".php",
+        "dart": ".dart",
+        "scala": ".scala",
+        "elixir": ".ex",
+        "erlang": ".erl",
+        "racket": ".rkt",
+    }
+
